4be0f6856749449fda5957ae40088fb77cd47bf2
linguo <linguo@ebay.com>
2015-09-08 17:37:12 +0800

    adjust deals upload components

--- promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/dev/PromoInitConfig.java
+++ promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/dev/PromoInitConfig.java
@@ -7,7 +7,8 @@ public class PromoInitConfig extends BasePromoInitConfig{
 
 	@Override
 	public String getEndPoint() {
-		return "http://phx5qa01c-ee86.stratus.phx.qa.ebay.com:8080/promoservice/v1";
+//		return "http://phx5qa01c-ee86.stratus.phx.qa.ebay.com:8080/promoservice/v1";
+		return "http://localhost:9080/promoservice/v1";
 	}
 
 }
