6ce8d5c4a3ab60cba0b85be0fb2f58811ee4e29f
linguo <linguo@ebay.com>
2016-02-12 17:35:50 +0800

    add zh_HK error message; adjust to get sku once when validating.

--- promowebweb/src/main/java/com/ebay/raptor/promotion/excel/APACDealsListingSheetHandler.java
+++ promowebweb/src/main/java/com/ebay/raptor/promotion/excel/APACDealsListingSheetHandler.java
@@ -15,17 +15,17 @@ import com.ebay.raptor.promotion.validation.ValidationUtil;
 public class APACDealsListingSheetHandler extends SiteDealsListingSheetHandler<APACDealsListing> {
 
 	public APACDealsListingSheetHandler(SpringValidatorAdapter validator, DealsListingService dealsListingService,
-			String promoId, Long userId) {
+			String promoId, Long userId) throws PromoException {
 		this.validator = validator;
 		this.dealsListingService = dealsListingService;
 		this.promoId = promoId;
 		this.userId = userId;
 		this.clazz = APACDealsListing.class;
+		this.skus = dealsListingService.getSkusByPromotionId(promoId, userId);
 	}
 
 	@Override
 	protected boolean validateSKU(APACDealsListing listing, int rowIndex) throws InvalidCellValueException, PromoException {
-		List<Sku> skus = dealsListingService.getSkusByPromotionId(promoId, userId);
 		return ValidationUtil.validateSKU(listing.getSkuId(), listing.getSkuName(), skus, rowIndex);
 	}
 
@@ -46,4 +46,5 @@ public class APACDealsListingSheetHandler extends SiteDealsListingSheetHandler<A
 	protected DealsListingService dealsListingService;
 	private String promoId;
 	private Long userId;
+	private List<Sku> skus;
 }
