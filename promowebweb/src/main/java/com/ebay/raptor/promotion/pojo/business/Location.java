8e65dc38593ee1807a25ef0ded4242418f8684a6
linguo <linguo@ebay.com>
2016-02-24 08:19:52 +0800

    revert to add more location.

--- promowebweb/src/main/java/com/ebay/raptor/promotion/pojo/business/Location.java
+++ promowebweb/src/main/java/com/ebay/raptor/promotion/pojo/business/Location.java
@@ -1,11 +1,14 @@
 package com.ebay.raptor.promotion.pojo.business;
 
 public enum Location implements IDescription{
+	US  ("US"),
+	CA  ("CA"),
 	UK	("UK"),
 	DE	("DE"),
 	ESP	("ESP"),
 	IT	("IT"),
 	FR	("FR"),
+	AU  ("AU"),
 	CN	("CN"),
 	HK	("HK"),
 	TW	("TW");
