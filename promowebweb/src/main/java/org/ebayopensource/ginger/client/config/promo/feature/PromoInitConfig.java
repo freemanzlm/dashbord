442dae7e1306f4d35b7ae8737709099271d7f5a8
Tuxiaoyue <xiatu@ebay.com>
2016-08-15 16:20:43 +0800

    change qa promocamp url

--- promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/feature/PromoInitConfig.java
+++ promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/feature/PromoInitConfig.java
@@ -7,7 +7,7 @@ public class PromoInitConfig extends BasePromoInitConfig{
 
 	@Override
 	public String getEndPoint() {
-		return "http://phx5qa01c-ee86.stratus.phx.qa.ebay.com:8080/promoservice/v1";
+		return "http://monterey-3699.slc01.dev.ebayc3.com:8080/promoservice/v1";
 //		return "http://promocamp-2.stratus.qa.ebay.com/promoservice/v1";
 //		return "http://localhost:9080/promoservice/v1";
 	}
