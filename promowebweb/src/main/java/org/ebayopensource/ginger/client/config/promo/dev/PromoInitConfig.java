5c2c80f4cdefda2a548b83030797dd17202c87e2
Tuxiaoyue <xiatu@ebay.com>
2016-12-19 16:37:21 +0800

    first commit

--- promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/dev/PromoInitConfig.java
+++ promowebweb/src/main/java/org/ebayopensource/ginger/client/config/promo/dev/PromoInitConfig.java
@@ -12,9 +12,9 @@ public class PromoInitConfig extends BasePromoInitConfig{
 	public String getEndPoint() {
 //		return "http://lm-shc-16501085:8080/promoservice/v1";
 //		return "http://10.249.74.112:7080/promoservice/v1";
-//		return "http://l-shc-15008105.corp.ebay.com:9090/promoservice/v1";
+		return "http://l-shc-15008105.corp.ebay.com:9090/promoservice/v1";
 //		return "http://phx5qa01c-ee86.stratus.phx.qa.ebay.com:8080/promoservice/v1";
-		return "http://monterey-3699.slc01.dev.ebayc3.com:8080/promoservice/v1";
+//		return "http://monterey-3699.slc01.dev.ebayc3.com:8080/promoservice/v1";
 	}
 	
 	/*@Override
