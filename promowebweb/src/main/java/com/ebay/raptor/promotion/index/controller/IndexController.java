1cbdaff98c810a0d40ac6d40b0609a128383cb25
Linus Yan <lyan2@ebay.com>
2017-06-06 19:31:22 +0800

    revert don't go to error page no matter whether there is legal term.

--- promowebweb/src/main/java/com/ebay/raptor/promotion/index/controller/IndexController.java
+++ promowebweb/src/main/java/com/ebay/raptor/promotion/index/controller/IndexController.java
@@ -174,10 +174,14 @@ public class IndexController {
 				model.addObject("subsidy", subsidy);
 				if (promo.getRewardType() != null && promo.getRewardType() > 0) {
 					SubsidyLegalTerm subsidyTerm = subsidyService.getSubsidyLegalTerm(promo.getRewardType(), promo.getRegion());
-					if (subsidyTerm != null && subsidyTerm.getSubsidyType() == 2) {
-						putWltAccountInfo(model, userData.getUserName(), null);
+					if (subsidyTerm == null) {
+						response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, getMessage(PromoError.SUBSIDY_LEGALTERM_NOT_FOUND.getKey()));
+					} else {
+						if (subsidyTerm != null && subsidyTerm.getSubsidyType() == 2) {
+							putWltAccountInfo(model, userData.getUserName(), null);
+						}
+						model.addObject("subsidyTerm", subsidyTerm);
 					}
-					model.addObject("subsidyTerm", subsidyTerm);
 				}
 			}
 		} else {
